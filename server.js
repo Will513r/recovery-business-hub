0d918cbd81c2388725d56f4e38158111414b28f8
William <will513r@gmail.com>
2026-04-01 14:57:46 -0400

    update server

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.use(express.static(path.join(__dirname, "public")));
 // 5. Set up the MySQL Database Connection using Environment Variables
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
